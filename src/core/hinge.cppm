7ae8a1300e50ee396b6a7406cc9bda7c59d23541
CorpoWolf <gmangavin1@gmail.com>
2026-02-25 10:37:52 -0800

    hinge function added

---
+++ src/core/hinge.cppm
@@ -0,0 +1,17 @@
+module;
+#include <vector>
+#include <ranges>
+
+export module hinge;
+import weretype;
+
+export auto HingeCTC(f32 height, u64 hingeQty) -> std::vector<f32> {
+	std::vector<f32> prepLoc(hingeQty);
+	f32 ctc = (height - (9*2)) / (hingeQty - 1);
+
+	for (int i : std::views::iota(0, as<int>(hingeQty))) {
+		prepLoc[i] = 9 + (ctc * i);
+	}
+
+	return prepLoc;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <print>
 import weretype;
 import modelgen;
+import hinge;
 
 int main() {
-	std::println("test");
-
 	const u8 norm = std::to_underlying(modelgen::dirEnum::y) | std::to_underlying(modelgen::dirEnum::n);
-	std::println("Test: {}", modelgen::directionVN(norm));
+	std::println("Normal direction Test: {}", modelgen::directionVN(norm));
+
+	auto vec = HingeCTC(84, 3);
+	std::println("{}", vec);
 	
 	if (auto r = modelgen::WriteObj("./.out/obj/genSlab.obj", modelgen::SlabModel(36, 84, 1.75)); !r) {
 		std::println("Failed: {}", r.error());
